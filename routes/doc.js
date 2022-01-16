8afa6ddf6e74d8d2efdb113741e008f53b16ec09
nattapong <bozshijack@gmail.com>
2022-01-16 11:43:06 +0700

    fix single qoute

--- routes/doc.js
+++ routes/doc.js
@@ -2,43 +2,43 @@ const path = require("path");
 
 const routeDoc = [
     {
-        method: 'GET',
-        path: '/documentation',
+        method: "GET",
+        path: "/documentation",
         handler: (request, reply) => {
             return reply.file(path.join(__dirname, "../spec/docs", "index.html"));
         }
     },
     {
-        method: 'GET',
-        path: '/bundle.css',
+        method: "GET",
+        path: "/bundle.css",
         handler: (request, reply) => {
             return reply.file(path.join(__dirname, "../spec/docs", "bundle.css"));
         }
     },
     {
-        method: 'GET',
-        path: '/bundle.js',
+        method: "GET",
+        path: "/bundle.js",
         handler: (request, reply) => {
             return reply.file(path.join(__dirname, "../spec/docs", "bundle.js"));
         }
     },
     {
-        method: 'GET',
-        path: '/insomnia.json',
+        method: "GET",
+        path: "/insomnia.json",
         handler: (request, reply) => {
             return reply.file(path.join(__dirname, "../spec/docs", "insomnia.json"));
         }
     },
     {
-        method: 'GET',
-        path: '/logo.png',
+        method: "GET",
+        path: "/logo.png",
         handler: (request, reply) => {
             return reply.file(path.join(__dirname, "../spec/docs", "logo.png"));
         }
     },
     {
-        method: 'GET',
-        path: '/favicon.ico',
+        method: "GET",
+        path: "/favicon.ico",
         handler: (request, reply) => {
             return reply.file(path.join(__dirname, "../spec/docs", "favicon.ico"));
         }
