e2a22f41337096da2c2bc5d3e54e3d4a1bade19f
nattapong <bozshijack@gmail.com>
2022-01-16 11:43:44 +0700

    use handle.directory to serve static files

--- routes/assignment2.js
+++ routes/assignment2.js
@@ -3,16 +3,11 @@ const path = require("path");
 const routeAssignment2 = [
     {
         method: "GET",
-        path: "/",
-        handler: (request, reply) => {
-            return reply.file(path.join(__dirname, "../public", "index.html"));
-        }
-    },
-    {
-        method: "GET",
-        path: "/index.js",
-        handler: (request, reply) => {
-            return reply.file(path.join(__dirname, "../public", "index.js"));
+        path: "/assignment2/{param*}",
+        handler: {
+            directory: {
+                path: path.join(__dirname, "../public")
+            }
         }
     }
 ];
