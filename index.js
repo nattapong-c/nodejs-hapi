320b1a671a807be43f1a0058b3093ed693dcbf00
nattapong <bozshijack@gmail.com>
2022-01-17 09:25:04 +0700

    set server to index

--- index.js
+++ index.js
@@ -1,7 +1,22 @@
-const server = require("./lib/server");
+const Hapi = require("@hapi/hapi");
+const inert = require("@hapi/inert");
+const routes = require("./utils/routes");
 
-server.start();
+const server = Hapi.server({
+    port: 3000,
+    host: 'localhost'
+});
+
+const start = async () => {
+    await server.register(inert);
+    server.route(routes());
+    await server.start();
+    return server;
+};
+
 process.on('unhandledRejection', (err) => {
     console.log(err);
     process.exit(1);
 });
+
+start();
